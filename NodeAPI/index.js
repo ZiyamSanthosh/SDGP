0dafbd5b85ccfc3b6fc049f91ff830f905f58bce
farheen-dev <55763160+farheen-dev@users.noreply.github.com>
2020-04-23 22:58:11 +0530

    Update index

--- NodeAPI/index.js
+++ NodeAPI/index.js
@@ -1,73 +1,71 @@
 // entry point of app
 
 // Import express
-let express = require('express');
+let express = require("express");
 // Import Body parser
-let bodyParser = require('body-parser');
+let bodyParser = require("body-parser");
 // Import Mongoose
-let mongoose = require('mongoose');
-// Import cors 
-var cors = require('cors');
+let mongoose = require("mongoose");
+// Import cors
+var cors = require("cors");
 // Initialize the app
 let app = express();
-//enable cors to all routes 
-app.use(cors());
+
+
 
 // Import routes
 let apiRoutes = require("./routes/api-routes");
 // Configure bodyparser to handle post requests
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
 app.use(bodyParser.json());
+//enable cors to all routes
+app.use(cors({ origin: "*" }));
 
-
-const httpErr = require('./models/httpErr');
-
-
+const httpErr = require("./models/httpErr");
 
 //middleware for error handling
 app.use((err, req, res, next) => {
-    if (res.headerSend) {
-        return next(err);
-    }
-    res.status(err.code || 500);
-    res.json({ message: err.message || "An unknown error occured" });
-})
-
-// Connect to Mongoose and set connection variable
-mongoose.connect('mongodb://localhost/DataBase', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+  if (res.headerSend) {
+    return next(err);
+  }
+  res.status(err.code || 500);
+  res.json({ message: err.message || "An unknown error occured" });
 });
 
+// Connect to Mongoose and set connection variable
+mongoose.connect("mongodb://localhost/DataBase", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 var db = mongoose.connection;
 
 // Added check for DB connection
 
-if (!db)
-    console.log("Error connecting db")
-else
-    console.log("Db connected successfully")
+if (!db) console.log("Error connecting db");
+else console.log("Db connected successfully");
 
 // Setup server port
 var port = process.env.PORT || 8000;
 
 // Send message for default URL
-app.get('/', (req, res) => res.send('Weekly tracking page is loaded'));
+app.get("/", (req, res) => res.send("Weekly tracking page is loaded"));
 
 // Use Api routes in the App
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
 // httperr
 app.use((req, res, next) => {
-    return next(new httpErr("Could not find route", 404));
+  return next(new httpErr("Could not find route", 404));
 });
-
 
 // Launch app to listen to specified port
 app.listen(port, function () {
-    console.log("Running Database on port " + port);
+  console.log("Running Database on port " + port);
 });
