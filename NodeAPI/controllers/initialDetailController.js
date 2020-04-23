eff6473afecc0c4809c99ca1cd90450adbc8e861
farheen-dev <55763160+farheen-dev@users.noreply.github.com>
2020-04-23 06:42:52 +0530

    Initial User Details Storing  in DB and Prediction Functionality Completed

--- NodeAPI/controllers/initialDetailController.js
+++ NodeAPI/controllers/initialDetailController.js
@@ -20,7 +20,8 @@ exports.getAll = async function (req, res) {
   }
 };
 
-//GET Back intial detail related to specific user id
+
+//GET Back initial detail related to specific user id
 exports.getById = async function (req, res) {
   try {
     var allData = await Post.find({});
@@ -35,3 +36,160 @@ exports.getById = async function (req, res) {
     return res.status(400).json({ message: err });
   }
 };
+
+
+/*POST ROUTE-------------------------------------*/
+
+//POST Route to send the initial form data to the database
+
+exports.postInitialDetails = function (req, res) {
+  //console.log(req.body);
+
+  //Initial Form details which are sending for the flask API 
+  let postData = {
+    Age: age(req.body.dob),
+    BMI: Number.parseFloat(bmi.bmi(req.body.height, req.body.weight).bmi),
+    Breast_Feeding: yesNoConversion(req.body.breastFeeding),
+    Marital_Status: maritalStatus(req.body.maritalStatus),
+    Alcohol: yesNoConversion(req.body.alcohol),
+    Smoking: yesNoConversion(req.body.smoking),
+    Breast_Cancer_History: yesNoConversion(req.body.breastCancerHistory),
+    Age_at_first_period: req.body.ageAtFirstPeriod,
+    Menstrual_Cycle: menstrualCycle(req.body.menstrualCycle),
+  };
+
+
+  const data = postData;
+ // let predictionValue;
+
+ //Calling getPrediction function
+  let prediction = getPrediction(data)
+    .then((response) => {
+      console.log(response.data);
+
+      //Initial Details stored in the DB (Converted Categorical data and user-define functions)
+      const post = new Post({
+        UserID: req.body.userId,
+        Age: age(req.body.dob),
+        BMI: bmi.bmi(req.body.height, req.body.weight).bmi,
+        Breast_Feeding: yesNoConversion(req.body.breastFeeding),
+        Marital_Status: maritalStatus(req.body.maritalStatus),
+        Alcohol: yesNoConversion(req.body.alcohol),
+        Smoking: yesNoConversion(req.body.smoking),
+        Breast_Cancer_History: yesNoConversion(req.body.breastCancerHistory),
+        Age_at_first_period: req.body.ageAtFirstPeriod,
+        Menstrual_Cycle: menstrualCycle(req.body.menstrualCycle),
+        Prediction: response.data,
+      });
+
+      //Sending initial details to the Track Collection in the DB
+      var Track = new Model();
+
+      Track.UserID = req.body.userId;
+      Track.Age = age(req.body.dob);
+      Track.BMI = bmi.bmi(req.body.height, req.body.weight).bmi;
+      Track.Marital_Status = maritalStatus(req.body.maritalStatus);
+      Track.Breast_Cancer_History = yesNoConversion(
+        req.body.breastCancerHistory
+      );
+      Track.Smoking = yesNoConversion(req.body.smoking);
+      Track.Alcohol = yesNoConversion(req.body.alcohol);
+      Track.BreastFeeding = yesNoConversion(req.body.breastFeeding);
+      Track.Age_at_first_period = req.body.ageAtFirstPeriod;
+      Track.Menstrual_Cycle = menstrualCycle(req.body.menstrualCycle);
+
+      const savedPost = post.save().catch((err) => {
+        return res.status(400).json({ message: err });
+      });
+
+      const save = Track.save().catch((err) => {
+        return res.status(400).json({ message: err });
+      });
+
+      //Returning the result status for predictions (intial details)
+      return res.send({ result: predictionRange(response.data) });
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).send("Something went wrong");
+    });
+};
+
+
+/*Using AXIOS to get HTTP request------------------------------------*/
+
+const getPrediction = (data) => {
+  try {
+    return axios.post("http://localhost:5000/predict", data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+
+/*FUNCTION FOR CALCULATING THE AGE------------------------------------*/
+
+function getYears(x) {
+  return Math.floor(x / 1000 / 60 / 60 / 24 / 365);
+}
+
+function age(doc) {
+  //  console.log(doc);
+  let n = Date.now();
+  let d = new Date(doc);
+  doc = getYears(n - d);
+  return doc;
+}
+
+/*FUNCTION FOR CONVERSION OF INPUTS------------------------------------*/
+
+//Function for conversion of YES - NO inputs
+function yesNoConversion(doc) {
+  if (doc.toString().toLowerCase() === "yes") {
+    doc = 1;
+  } else {
+    doc = 0;
+  }
+  return doc;
+}
+
+//Function for conversion of HAS-CYCLE - NO-CYCLE inputs
+function menstrualCycle(doc) {
+  if (doc.toString().toLowerCase() === "hascycle") {
+    doc = 1;
+  } else {
+    doc = 0;
+  }
+  return doc;
+}
+
+//Function for conversion of MARRIED-TOGETHER-SINGLE inputs
+function maritalStatus(doc) {
+  if (doc.toString().toLowerCase() === "married") {
+    doc = 1;
+  } else if (doc.toString().toLowerCase() === "together") {
+    doc = 2;
+  } else {
+    doc = 3;
+  }
+  return doc;
+}
+
+//Function for creating status for prediction value ranges
+function predictionRange(doc) {
+  val = Number.parseFloat(doc);
+  let status;
+  if (0 <= val && val <= 20) {
+    status = 1;
+  } else if (20 < val && val <= 40) {
+    status = 2;
+  } else if (40 < val && val <= 60) {
+    status = 3;
+  } else if (60 < val && val <= 80) {
+    status = 4;
+  } else if (80 < val && val <= 100) {
+    status = 5;
+  }
+  return status;
+}
--- NodeAPI/routes/api-routes.js
+++ NodeAPI/routes/api-routes.js
@@ -1,39 +1,40 @@
 // api-routes.js
 // Initialize express router
-let router = require('express').Router();
+let router = require("express").Router();
 // Set default API response
-router.get('/', function (req, res) {
-    res.json({
-        status: 'API Its Working',
-        message: 'Welcome to RESTHub crafted with love!',
-    });
+router.get("/", function (req, res) {
+  res.json({
+    status: "API Its Working",
+    message: "Welcome to RESTHub crafted with love!",
+  });
 });
 // Import trackingController
-var trackingController = require('../controllers/trackingController');
+var trackingController = require("../controllers/trackingController");
 // Import initialDetailController
 var initialDetailController = require("../controllers/initialDetailController");
 // track routes
-router.route('/track')
-    .get(trackingController.index) //route for getting the average prediction for curent date
-    .post(trackingController.new) //route for updating daily tracking details
-    .put(trackingController.call) //rout for getting the artical
-    .patch(trackingController.getAllPrediction); //rout for getting all past predictions
+router
+  .route("/track")
+  .get(trackingController.index) //route for getting the average prediction for curent date
+  .post(trackingController.new) //route for updating daily tracking details
+  .put(trackingController.call) //rout for getting the artical
+  .patch(trackingController.getAllPrediction); //rout for getting all past predictions
 
 router
-    .route("/track/:track_id")
-    .get(trackingController.view)    // additional route. not used for any functionality (used for viewing a data by giving id)
-    .delete(trackingController.delete);   // additional route. not used for any functionality (used for delete a data by giving id)
+  .route("/track/:track_id")
+  .get(trackingController.view) // additional route. not used for any functionality (used for viewing a data by giving id)
+  .delete(trackingController.delete); // additional route. not used for any functionality (used for delete a data by giving id)
 
 //Initial Data Route
 router
-  .route("/detail")    
-  .get(initialDetailController.getAll)      // Route for get all documents related to inital predcition
+  .route("/detail")
+  .get(initialDetailController.getAll) // Route for get all documents related to initial prediction
+  .post(initialDetailController.postInitialDetails); // Route for inserting initial form details to the DB
+                                                    // and getting the initial prediction
 
-  router
-  .route("/detail/:user_id") 
-  .get(initialDetailController.getById)        // Route for getting intial prediction details related to preticular userid
- 
-
+router
+.route("/detail/:user_id")
+.get(initialDetailController.getById); // Route for getting initial prediction details related to preticular userid
 
 // Export API routes
 module.exports = router;
